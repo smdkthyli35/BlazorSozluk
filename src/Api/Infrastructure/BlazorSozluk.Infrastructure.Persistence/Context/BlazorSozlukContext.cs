684ab80260ee8ded38c76294647f7002efbefad6
zeljkosamed <smdkthyli@gmail.com>
2022-05-09 22:07:42 +0300

    SaveChanges interceptors added.

--- src/Api/Infrastructure/BlazorSozluk.Infrastructure.Persistence/Context/BlazorSozlukContext.cs
+++ src/Api/Infrastructure/BlazorSozluk.Infrastructure.Persistence/Context/BlazorSozlukContext.cs
@@ -3,7 +3,9 @@ using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace BlazorSozluk.Infrastructure.Persistence.Context
@@ -24,5 +26,52 @@ namespace BlazorSozluk.Infrastructure.Persistence.Context
         public DbSet<EntryCommentVote> EntryCommentVotes { get; set; }
         public DbSet<EntryCommentFavorite> EntryCommentFavorites { get; set; }
         public DbSet<EmailConfirmation> EmailConfirmations { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        }
+
+        public override int SaveChanges()
+        {
+            OnBeforeSave();
+            return base.SaveChanges();
+        }
+
+        public override int SaveChanges(bool acceptAllChangesOnSuccess)
+        {
+            OnBeforeSave();
+            return base.SaveChanges(acceptAllChangesOnSuccess);
+        }
+
+        public override Task<int> SaveChangesAsync(bool acceptAllChangesOnSuccess, CancellationToken cancellationToken = default)
+        {
+            OnBeforeSave();
+            return base.SaveChangesAsync(acceptAllChangesOnSuccess, cancellationToken);
+        }
+
+        public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+        {
+            OnBeforeSave();
+            return base.SaveChangesAsync(cancellationToken);
+        }
+
+        private void OnBeforeSave()
+        {
+            var addedEntities = ChangeTracker.Entries()
+                                             .Where(i => i.State == EntityState.Added)
+                                             .Select(i => (BaseEntity)i.Entity);
+
+            PrepareAddedEntities(addedEntities);
+        }
+
+        private void PrepareAddedEntities(IEnumerable<BaseEntity> entities)
+        {
+            foreach (var entity in entities)
+            {
+                if (entity.CreateDate == DateTime.MinValue)
+                    entity.CreateDate = DateTime.Now;
+            }
+        }
     }
 }
