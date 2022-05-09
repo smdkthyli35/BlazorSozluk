54136259a04caabab4c5cfd0c034e1184d0ccba1
zeljkosamed <smdkthyli@gmail.com>
2022-05-09 21:31:38 +0300

    Database classes created.

---
+++ src/Api/Core/BlazorSozluk.Api.Domain/Models/EntryCommentVote.cs
@@ -0,0 +1,18 @@
+﻿using BlazorSozluk.Common.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorSozluk.Api.Domain.Models
+{
+    public class EntryCommentVote : BaseEntity
+    {
+        public Guid EntryCommentId { get; set; }
+        public Guid CreatedById { get; set; }
+
+        public VoteType VoteType { get; set; }
+        public virtual EntryComment EntryComment { get; set; }
+    }
+}
