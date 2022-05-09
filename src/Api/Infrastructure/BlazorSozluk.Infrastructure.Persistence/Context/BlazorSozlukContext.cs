ea7ec04dbd20df35573f7b117c6704f87f29fa7b
zeljkosamed <smdkthyli@gmail.com>
2022-05-09 21:53:50 +0300

    DbContext and DbSets added.

---
+++ src/Api/Infrastructure/BlazorSozluk.Infrastructure.Persistence/Context/BlazorSozlukContext.cs
@@ -0,0 +1,28 @@
+﻿using BlazorSozluk.Api.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorSozluk.Infrastructure.Persistence.Context
+{
+    public class BlazorSozlukContext : DbContext
+    {
+        public const string DEFAULT_SCHEMA = "dbo";
+
+        public BlazorSozlukContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Entry> Entries { get; set; }
+        public DbSet<EntryVote> EntryVotes { get; set; }
+        public DbSet<EntryFavorite> EntryFavorites { get; set; }
+        public DbSet<EntryComment> EntryComments { get; set; }
+        public DbSet<EntryCommentVote> EntryCommentVotes { get; set; }
+        public DbSet<EntryCommentFavorite> EntryCommentFavorites { get; set; }
+        public DbSet<EmailConfirmation> EmailConfirmations { get; set; }
+    }
+}
