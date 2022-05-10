e7f283befae5136038084eaf88992d1310823235
zeljkosamed <smdkthyli@gmail.com>
2022-05-10 22:27:02 +0300

    PasswordEncryptor and SeedData, GetUser method added.

---
+++ src/Api/Infrastructure/BlazorSozluk.Infrastructure.Persistence/Context/SeedData.cs
@@ -0,0 +1,30 @@
+﻿using BlazorSozluk.Api.Domain.Models;
+using BlazorSozluk.Common.Infrastructure;
+using Bogus;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorSozluk.Infrastructure.Persistence.Context
+{
+    internal class SeedData
+    {
+        private static List<User> GetUsers()
+        {
+            var result = new Faker<User>("tr")
+                .RuleFor(i => i.Id, i => Guid.NewGuid())
+                .RuleFor(i => i.CreateDate, i => i.Date.Between(DateTime.Now.AddDays(-100), DateTime.Now))
+                .RuleFor(i => i.FirstName, i => i.Person.FirstName)
+                .RuleFor(i => i.LastName, i => i.Person.LastName)
+                .RuleFor(i => i.EmailAddress, i => i.Internet.Email())
+                .RuleFor(i => i.UserName, i => i.Internet.UserName())
+                .RuleFor(i => i.Password, i => PasswordEncryptor.Encrpt(i.Internet.Password()))
+                .RuleFor(i => i.EmailConfirmed, i => i.PickRandom(true, false))
+                .Generate(500);
+
+            return result;
+        }
+    }
+}
