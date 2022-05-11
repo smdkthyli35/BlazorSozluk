2e71090ec306fa7974374211b636f23f887db9ed
zeljkosamed <smdkthyli@gmail.com>
2022-05-11 21:13:06 +0300

    AddApplicationRegistration added.

--- src/Api/WebApi/BlazorSozluk.Api.WebApi/Startup.cs
+++ src/Api/WebApi/BlazorSozluk.Api.WebApi/Startup.cs
@@ -1,3 +1,4 @@
+using BlazorSozluk.Api.Application.Extensions;
 using BlazorSozluk.Infrastructure.Persistence.Extensions;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -27,10 +28,11 @@ namespace BlazorSozluk.Api.WebApi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddControllers();
 
+            services.AddApplicationRegistration();
             services.AddInfrastructureRegistration(Configuration);
 
-            services.AddControllers();
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "BlazorSozluk.Api.WebApi", Version = "v1" });
