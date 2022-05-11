ca2e8b53e5d0682b8f87c6cf172d36538dcadb4a
zeljkosamed <smdkthyli@gmail.com>
2022-05-11 14:50:44 +0300

    Added IGenericRepository.

---
+++ src/Api/Core/BlazorSozluk.Api.Application/Interfaces/Repositories/IEntryRepository.cs
@@ -0,0 +1,13 @@
+﻿using BlazorSozluk.Api.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorSozluk.Api.Application.Interfaces.Repositories
+{
+    public interface IEntryRepository : IGenericRepository<Entry>
+    {
+    }
+}
