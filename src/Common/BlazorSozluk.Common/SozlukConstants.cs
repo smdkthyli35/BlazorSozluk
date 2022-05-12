82d1ac01f7c17745229b05285b4237396af0a3e1
zeljkosamed <smdkthyli@gmail.com>
2022-05-12 21:07:24 +0300

    QueueFactory added.

---
+++ src/Common/BlazorSozluk.Common/SozlukConstants.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorSozluk.Common
+{
+    public class SozlukConstants
+    {
+        public const string RabbitMQHost = "localhost";
+        public const string DefaultExchangeType = "direct";
+    }
+}
