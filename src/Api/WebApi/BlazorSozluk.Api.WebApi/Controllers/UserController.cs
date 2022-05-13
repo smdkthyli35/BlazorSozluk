028f71d038a1f6219e6c6cfbbeebc077d1ec477d
zeljkosamed <smdkthyli@gmail.com>
2022-05-13 20:03:09 +0300

    ChangeUserPasswordCommand added.

--- src/Api/WebApi/BlazorSozluk.Api.WebApi/Controllers/UserController.cs
+++ src/Api/WebApi/BlazorSozluk.Api.WebApi/Controllers/UserController.cs
@@ -31,16 +31,16 @@ namespace BlazorSozluk.Api.WebApi.Controllers
         [HttpPost]
         public async Task<IActionResult> Create([FromBody] CreateUserCommand command)
         {
-            var quid = await mediator.Send(command);
-            return Ok(quid);
+            var guid = await mediator.Send(command);
+            return Ok(guid);
         }
 
         [HttpPost]
         [Route("Update")]
         public async Task<IActionResult> Update([FromBody] UpdateUserCommand command)
         {
-            var quid = await mediator.Send(command);
-            return Ok(quid);
+            var guid = await mediator.Send(command);
+            return Ok(guid);
         }
     }
 }
