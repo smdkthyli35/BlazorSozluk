94423c821cdf732a2b299097b1d8ee25668cfef9
zeljkosamed <smdkthyli@gmail.com>
2022-05-13 20:10:31 +0300

    ChangeUserPasswordCommandHandler added.

---
+++ src/Api/Core/BlazorSozluk.Api.Application/Features/Commands/User/ChangePassword/ChangeUserPasswordCommandHandler.cs
@@ -0,0 +1,46 @@
+﻿using AutoMapper;
+using BlazorSozluk.Api.Application.Interfaces.Repositories;
+using BlazorSozluk.Common.Infrastructure;
+using BlazorSozluk.Common.Infrastructure.Exceptions;
+using BlazorSozluk.Common.Models.RequestModels;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace BlazorSozluk.Api.Application.Features.Commands.User.ChangePassword
+{
+    public class ChangeUserPasswordCommandHandler : IRequestHandler<ChangeUserPasswordCommand, bool>
+    {
+        private readonly IUserRepository userRepository;
+
+        public ChangeUserPasswordCommandHandler(IUserRepository userRepository)
+        {
+            this.userRepository = userRepository;
+        }
+
+        public async Task<bool> Handle(ChangeUserPasswordCommand request, CancellationToken cancellationToken)
+        {
+            if (!request.UserId.HasValue)
+                throw new ArgumentNullException(nameof(request.UserId));
+
+            var dbUser = await userRepository.GetByIdAsync(request.UserId.Value);
+
+            if (dbUser is null)
+                throw new DatabaseValidationException("User not found!");
+
+            var encPass = PasswordEncryptor.Encrpt(request.OldPassword);
+            if (dbUser.Password != encPass)
+                throw new DatabaseValidationException("Old password wrong!");
+
+            dbUser.Password = encPass;
+
+            await userRepository.UpdateAsync(dbUser);
+
+            return true;
+        }
+    }
+}
