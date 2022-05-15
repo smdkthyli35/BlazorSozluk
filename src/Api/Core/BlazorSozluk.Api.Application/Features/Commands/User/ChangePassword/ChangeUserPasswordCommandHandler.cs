3ae3ef4fc03c8e88a4456bcc8fe7ee1bdc7bcb94
zeljkosamed <smdkthyli@gmail.com>
2022-05-15 15:59:21 +0300

    GetEntriesQuery and GetEntriesViewModel added.

--- src/Api/Core/BlazorSozluk.Api.Application/Features/Commands/User/ChangePassword/ChangeUserPasswordCommandHandler.cs
+++ src/Api/Core/BlazorSozluk.Api.Application/Features/Commands/User/ChangePassword/ChangeUserPasswordCommandHandler.cs
@@ -36,7 +36,7 @@ namespace BlazorSozluk.Api.Application.Features.Commands.User.ChangePassword
             if (dbUser.Password != encPass)
                 throw new DatabaseValidationException("Old password wrong!");
 
-            dbUser.Password = encPass;
+            dbUser.Password = PasswordEncryptor.Encrpt(request.NewPassword);
 
             await userRepository.UpdateAsync(dbUser);
 
