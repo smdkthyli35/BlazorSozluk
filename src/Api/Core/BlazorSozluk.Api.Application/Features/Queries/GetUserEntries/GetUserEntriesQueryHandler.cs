fae264e42ee0645c6d2218fc4f73031450920a72
zeljkosamed <smdkthyli@gmail.com>
2022-05-23 17:45:04 +0300

    GetUserEntriesQueryHandler added.

---
+++ src/Api/Core/BlazorSozluk.Api.Application/Features/Queries/GetUserEntries/GetUserEntriesQueryHandler.cs
@@ -0,0 +1,59 @@
+﻿using BlazorSozluk.Api.Application.Interfaces.Repositories;
+using BlazorSozluk.Common.Infrastructure.Extensions;
+using BlazorSozluk.Common.Models.Page;
+using BlazorSozluk.Common.Models.Queries;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace BlazorSozluk.Api.Application.Features.Queries.GetUserEntries
+{
+    public class GetUserEntriesQueryHandler : IRequestHandler<GetUserEntriesQuery, PagedViewModel<GetUserEntriesDetailViewModel>>
+    {
+        private readonly IEntryRepository entryRepository;
+
+        public GetUserEntriesQueryHandler(IEntryRepository entryRepository)
+        {
+            this.entryRepository = entryRepository;
+        }
+
+        public async Task<PagedViewModel<GetUserEntriesDetailViewModel>> Handle(GetUserEntriesQuery request, CancellationToken cancellationToken)
+        {
+            var query = entryRepository.AsQueryable();
+
+            if (request.UserId != null && request.UserId.HasValue && request.UserId != Guid.Empty)
+            {
+                query = query.Where(i => i.CreatedById == request.UserId);
+            }
+            else if (!string.IsNullOrEmpty(request.UserName))
+            {
+                query = query.Where(i => i.CreatedBy.UserName == request.UserName);
+            }
+            else
+                return null;
+
+            query = query.Include(i => i.EntryFavorites)
+                         .Include(i => i.CreatedBy);
+
+            var list = query.Select(i => new GetUserEntriesDetailViewModel()
+            {
+                Id = i.Id,
+                Subject = i.Subject,
+                Content = i.Content,
+                IsFavorited = request.UserId.HasValue && i.EntryFavorites.Any(j => j.CreatedById == request.UserId),
+                FavoritedCount = i.EntryFavorites.Count,
+                CreatedDate = i.CreateDate,
+                CreatedByUserName = i.CreatedBy.UserName
+            });
+
+            var entries = await list.GetPaged(request.Page, request.PageSize);
+
+            return entries;
+        }
+    }
+}
