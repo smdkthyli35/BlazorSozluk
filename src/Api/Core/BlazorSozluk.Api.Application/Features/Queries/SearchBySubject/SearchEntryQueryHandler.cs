da9145c75f53a80229b2fcb2424328844807ed31
zeljkosamed <smdkthyli@gmail.com>
2022-05-23 17:55:17 +0300

    SearchEntryQueryHandler added.

---
+++ src/Api/Core/BlazorSozluk.Api.Application/Features/Queries/SearchBySubject/SearchEntryQueryHandler.cs
@@ -0,0 +1,36 @@
+﻿using BlazorSozluk.Api.Application.Interfaces.Repositories;
+using BlazorSozluk.Common.Models.Queries;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace BlazorSozluk.Api.Application.Features.Queries.SearchBySubject
+{
+    public class SearchEntryQueryHandler : IRequestHandler<SearchEntryQuery, List<SearchEntryViewModel>>
+    {
+        private readonly IEntryRepository entryRepository;
+
+        public SearchEntryQueryHandler(IEntryRepository entryRepository)
+        {
+            this.entryRepository = entryRepository;
+        }
+
+        public async Task<List<SearchEntryViewModel>> Handle(SearchEntryQuery request, CancellationToken cancellationToken)
+        {
+            var result = entryRepository
+                .Get(i => EF.Functions.Like(i.Subject, $"{request.SearchText}%"))
+                .Select(i => new SearchEntryViewModel()
+                {
+                    Id = i.Id,
+                    Subject = i.Subject
+                });
+
+            return await result.ToListAsync(cancellationToken);
+        }
+    }
+}
