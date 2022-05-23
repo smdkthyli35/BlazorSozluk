cfeb6258648dbca95e7b988ccd5b7c50e50beffd
zeljkosamed <smdkthyli@gmail.com>
2022-05-23 17:24:34 +0300

    UserDetailViewModel added.

---
+++ src/Common/BlazorSozluk.Common/Models/Queries/UserDetailViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorSozluk.Common.Models.Queries
+{
+    public class UserDetailViewModel
+    {
+        public Guid Id { get; set; }
+        public DateTime CreateDate { get; set; }
+        public string FİrstName { get; set; }
+        public string LastName { get; set; }
+        public string UserName { get; set; }
+        public string EmailAddress { get; set; }
+        public bool EmailConfirmed { get; set; }
+    }
+}
