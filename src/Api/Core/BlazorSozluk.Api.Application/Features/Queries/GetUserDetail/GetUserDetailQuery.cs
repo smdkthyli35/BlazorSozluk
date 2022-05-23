ed9ab84537212be57e53e4853a3f2fd6b78feedd
zeljkosamed <smdkthyli@gmail.com>
2022-05-23 17:26:45 +0300

    GetUserDetailQuery added.

---
+++ src/Api/Core/BlazorSozluk.Api.Application/Features/Queries/GetUserDetail/GetUserDetailQuery.cs
@@ -0,0 +1,22 @@
+﻿using BlazorSozluk.Common.Models.Queries;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorSozluk.Api.Application.Features.Queries.GetUserDetail
+{
+    public class GetUserDetailQuery : IRequest<UserDetailViewModel>
+    {
+        public Guid UserId { get; set; }
+        public string UserName { get; set; }
+
+        public GetUserDetailQuery(Guid userId, string userName = null)
+        {
+            UserId = userId;
+            UserName = userName;
+        }
+    }
+}
