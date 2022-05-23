065dc9168dffbf1eabb382947a0caaf9514d8ace
zeljkosamed <smdkthyli@gmail.com>
2022-05-23 18:21:45 +0300

    VoteController added.

--- src/Common/BlazorSozluk.Common/Models/RequestModels/CreateEntryVoteCommand.cs
+++ src/Common/BlazorSozluk.Common/Models/RequestModels/CreateEntryVoteCommand.cs
@@ -12,5 +12,12 @@ namespace BlazorSozluk.Common.Models.RequestModels
         public Guid EntryId { get; set; }
         public VoteType VoteType { get; set; }
         public Guid CreatedBy { get; set; }
+
+        public CreateEntryVoteCommand(Guid entryId, VoteType voteType, Guid createdBy)
+        {
+            EntryId = entryId;
+            VoteType = voteType;
+            CreatedBy = createdBy;
+        }
     }
 }
