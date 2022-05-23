d23e945647309ecce2c1e452e41e0ddf9c310c98
zeljkosamed <smdkthyli@gmail.com>
2022-05-23 17:37:50 +0300

    GetUserEntriesQuery added.

---
+++ src/Api/Core/BlazorSozluk.Api.Application/Features/Queries/GetUserEntries/GetUserEntriesQuery.cs
@@ -0,0 +1,23 @@
+﻿using BlazorSozluk.Common.Models.Page;
+using BlazorSozluk.Common.Models.Queries;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorSozluk.Api.Application.Features.Queries.GetUserEntries
+{
+    public class GetUserEntriesQuery : BasePagedQuery, IRequest<PagedViewModel<GetUserEntriesDetailViewModel>>
+    {
+        public Guid? UserId { get; set; }
+        public string UserName { get; set; }
+
+        public GetUserEntriesQuery(Guid? userId, string userName = null, int page = 1, int pageSize = 10) : base(page, pageSize)
+        {
+            UserId = userId;
+            UserName = userName;
+        }
+    }
+}
