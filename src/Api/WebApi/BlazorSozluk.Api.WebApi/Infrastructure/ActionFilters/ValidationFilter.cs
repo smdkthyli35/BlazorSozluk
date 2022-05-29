ae75c53f95874e17ba36ef6d60a8ede52fd3a037
zeljkosamed <smdkthyli@gmail.com>
2022-05-29 15:24:22 +0300

    ValidationFilter added.

---
+++ src/Api/WebApi/BlazorSozluk.Api.WebApi/Infrastructure/ActionFilters/ValidationFilter.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorSozluk.Api.WebApi.Infrastructure.ActionFilters
+{
+    public class ValidateModelStateFilter : IAsyncActionFilter
+    {
+        public async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
+        {
+            if (!context.ModelState.IsValid)
+            {
+                var messages = context.ModelState.Values.SelectMany(x => x.Errors)
+                                                                   .Select(x => !string.IsNullOrEmpty(x.ErrorMessage)
+                                                                   ? x.ErrorMessage
+                                                                   : x.Exception?.Message).Distinct().ToList();
+
+                return;
+            }
+
+            await next();
+        }
+    }
+}
