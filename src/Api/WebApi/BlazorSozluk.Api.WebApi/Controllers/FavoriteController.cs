f6da0bc9d63edeb5c892d4881f94eec0bd77150e
zeljkosamed <smdkthyli@gmail.com>
2022-08-15 17:14:44 +0300

    FavoriteController added.

---
+++ src/Api/WebApi/BlazorSozluk.Api.WebApi/Controllers/FavoriteController.cs
@@ -0,0 +1,56 @@
+﻿using BlazorSozluk.Api.Application.Features.Commands.Entry.CreateFav;
+using BlazorSozluk.Api.Application.Features.Commands.Entry.DeleteFav;
+using BlazorSozluk.Api.Application.Features.Commands.EntryComment.CreateFav;
+using BlazorSozluk.Api.Application.Features.Commands.EntryComment.DeleteFav;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Threading.Tasks;
+
+namespace BlazorSozluk.Api.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FavoriteController : BaseController
+    {
+        private readonly IMediator mediator;
+
+        public FavoriteController(IMediator mediator)
+        {
+            this.mediator = mediator;
+        }
+
+        [HttpPost]
+        [Route("entry/{entryId}")]
+        public async Task<IActionResult> CreateEntryFav(Guid entryId)
+        {
+            var result = await mediator.Send(new CreateEntryFavCommand(entryId, UserId));
+            return Ok(result);
+        }
+
+        [HttpPost]
+        [Route("entrycomment/{entrycommentId}")]
+        public async Task<IActionResult> CreateEntryCommentFav(Guid entryCommentId)
+        {
+            var result = await mediator.Send(new CreateEntryCommentFavCommand(entryCommentId, UserId.Value));
+            return Ok(result);
+        }
+
+        [HttpPost]
+        [Route("deleteentryfav/{entryId}")]
+        public async Task<IActionResult> DeleteEntryFav(Guid entryId)
+        {
+            var result = await mediator.Send(new DeleteEntryFavCommand(entryId, UserId.Value));
+            return Ok(result);
+        }
+
+        [HttpPost]
+        [Route("deleteentrycommentfav/{entryCommentId}")]
+        public async Task<IActionResult> DeleteEntryCommentFav(Guid entryCommentId)
+        {
+            var result = await mediator.Send(new DeleteEntryCommentFavCommand(entryCommentId, UserId.Value));
+            return Ok(result);
+        }
+    }
+}
